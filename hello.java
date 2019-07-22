8efb9c1c3597ec3e9d5c74f59e114e9051bc03a4
hari <harinath026@gmail.com>
2019-07-23 01:48:51 +0530

    file resetted

--- hello.java
+++ hello.java
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-class hello #from dev2
+
+class hello
 {
-	public static void main ()  #from dev2
+	public static void main ()
+	{
 		system.out.println("hello ");
-		system.out.println("hello world #from dev2");
-=======
-class hello #form hari
-{
-	public static void main () #from hari
-		system.out.println("hello hari #from hari ");
 		system.out.println("hello world");
->>>>>>> 5c33cb79ce163c3122ca76e6e5d440533cc3ad0a
+	}
 }
